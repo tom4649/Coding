05b5f7736bdb0454f73f0c0ecdfc8cc3bcb994f4
tom4649 <tkaaynakiyoshi@gmail.com>
2026-03-29 05:11:16 +0900

    Apply suggestion

--- 53/sol1.py
+++ 53/sol1.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = -float("inf")
-        cummulative_sum = 0
-        min_cummulative_sum = 0
+        cumulative_sum = 0
+        min_cumulative_sum = 0
         for n in nums:
-            cummulative_sum += n
-            max_sum = max(max_sum, cummulative_sum - min_cummulative_sum)
-            min_cummulative_sum = min(min_cummulative_sum, cummulative_sum)
+            cumulative_sum += n
+            max_sum = max(max_sum, cumulative_sum - min_cumulative_sum)
+            min_cumulative_sum = min(min_cumulative_sum, cumulative_sum)
 
         return max_sum
--- 53/sol2.py
+++ 53/sol2.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = -float("inf")
-        max_sum_end_with_current_n = -float("inf")
+        max_ending_here = -float("inf")
         for n in nums:
-            max_sum_end_with_current_n = max(max_sum_end_with_current_n + n, n)
-            max_sum = max(max_sum, max_sum_end_with_current_n)
+            max_ending_here = max(n, max_ending_here + n)
+            max_so_far = max(max_so_far, max_ending_here)
 
         return max_sum
