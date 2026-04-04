4d130878bd05535b69d4ee52676247d0fb88d73d
tom4649 <tkaaynakiyoshi@gmail.com>
2026-04-04 20:23:37 +0900

    Fix

--- 33/sol1.py
+++ 33/sol1.py
@@ -17,14 +17,17 @@ class Solution:
         minimum_index = left
 
         # Step 2: Find the target
-        left = 0  # (i - minimum_index) % len_nums < left  -> nums[i] < target
-        right = len_nums  # (i - minimum_index) % len_nums >= right -> nums[i] >= target
-        while left < right:
-            mid = left + (right - left) // 2
-            if nums[(mid + minimum_index) % len_nums] < target:
-                left = mid + 1
+        left_in_sorted = (
+            0  # (i - minimum_index) % len_nums < left_in_sorted  -> nums[i] < target
+        )
+        right_in_sorted = len_nums  # (i - minimum_index) % len_nums >= right_in_sorted -> nums[i] >= target
+        while left_in_sorted < right_in_sorted:
+            mid_in_sorted = left_in_sorted + (right_in_sorted - left_in_sorted) // 2
+            if nums[(mid_in_sorted + minimum_index) % len_nums] < target:
+                left_in_sorted = mid_in_sorted + 1
             else:
-                right = mid
+                right_in_sorted = mid_in_sorted
 
-        target_index = (left + minimum_index) % len_nums
-        return target_index if nums[target_index] == target else -1
+        if nums[(left_in_sorted + minimum_index) % len_nums] != target:
+            return -1
+        return (left_in_sorted + minimum_index) % len_nums
