dd1d0dde059238ac9a398c6582e0e2eb72585a85
tom4649 <tkaaynakiyoshi@gmail.com>
2026-04-05 19:40:09 +0900

    Change shifted to exponent

--- 50/sol1_revised.py
+++ 50/sol1_revised.py
@@ -1,11 +1,11 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         result = 1.0
-        shifted = abs(n)
+        exponent = abs(n)
         current_pow = x
-        while shifted > 0:
-            if shifted & 1:
+        while exponent > 0:
+            if exponent & 1:
                 result *= current_pow
-            shifted >>= 1
+            exponent >>= 1
             current_pow *= current_pow
         return result if n >= 0 else 1 / result
