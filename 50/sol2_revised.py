2ab6bef9c9a4c6818d7aa610fadb86f8a2aeba6d
tom4649 <tkaaynakiyoshi@gmail.com>
2026-04-12 19:01:12 +0900

    Add suggested changes

--- 50/sol2_revised.py
+++ 50/sol2_revised.py
@@ -3,7 +3,8 @@ class Solution:
         if n == 0:
             return 1.0
         if n < 0:
-            return 1.0 / self.myPow(x, -n)
+            x = 1.0 / x
+            n = -n
         sub_pow = self.myPow(x, n // 2)
         if n % 2 == 1:
             return sub_pow * sub_pow * x
