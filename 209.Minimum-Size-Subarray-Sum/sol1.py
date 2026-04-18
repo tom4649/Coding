28d07a4809e1043fc32ce522692c3f929f193e37
tom4649 <tkaaynakiyoshi@gmail.com>
2026-04-19 05:49:43 +0900

    Add suggested changes

--- 209.Minimum-Size-Subarray-Sum/sol1.py
+++ 209.Minimum-Size-Subarray-Sum/sol1.py
@@ -8,10 +8,11 @@ class Solution:
         min_subarray_len = float("inf")
         for right, num_right in enumerate(nums):
             current_sum += num_right
-            if current_sum >= target:
-                while left < len(nums) and current_sum - nums[left] >= target:
-                    current_sum -= nums[left]
-                    left += 1
-                min_subarray_len = min(min_subarray_len, right - left + 1)
+            if current_sum < target:
+                continue
+            while left < len(nums) and current_sum - nums[left] >= target:
+                current_sum -= nums[left]
+                left += 1
+            min_subarray_len = min(min_subarray_len, right - left + 1)
 
         return 0 if math.isinf(min_subarray_len) else min_subarray_len
