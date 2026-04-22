aced5facd899ed5aab52005f1905441702a12d86
tom4649 <tkaaynakiyoshi@gmail.com>
2026-04-22 12:33:14 +0900

    283. Move Zeroes

---
+++ 283.Move-Zeroes/sol2.py
@@ -0,0 +1,33 @@
+from unittest import skip
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        def skip_index(index, zero_skip=True):
+            if zero_skip:
+                while index < len(nums) and nums[index] == 0:
+                    index += 1
+            else:
+                while index < len(nums) and nums[index] != 0:
+                    index += 1
+            return index
+
+        scanning_index = 0
+        while scanning_index < len(nums):
+            first_zero_index = skip_index(scanning_index, zero_skip=False)
+            scanning_index = skip_index(first_zero_index, zero_skip=True)
+            if scanning_index == len(nums):
+                return
+            while nums[first_zero_index] == 0 and scanning_index < len(nums):
+                nums[scanning_index], nums[first_zero_index] = (
+                    nums[first_zero_index],
+                    nums[scanning_index],
+                )
+                scanning_index = skip_index(scanning_index, zero_skip=True)
+                first_zero_index += 1
+
+        return
