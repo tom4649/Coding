80fe4394668a07b83464e473aeb01cf7ae0e31d2
tom4649 <tkaaynakiyoshi@gmail.com>
2026-05-07 06:00:57 +0900

    11. Container With Most Water

---
+++ 0011.Container-With-Most-Water/sol2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        if len(height) < 2:
+            return 0
+
+        start = 0
+        last = len(height) - 1
+        max_area = 0
+
+        while start < last:
+            water_height = min(height[start], height[last])
+            max_area = max(max_area, (last - start) * water_height)
+            if height[start] < height[last]:
+                while start < last and height[start] <= water_height:
+                    start += 1
+            else:
+                while start < last and height[last] <= water_height:
+                    last -= 1
+
+        return max_area
