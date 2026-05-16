3ea612a6031f52bd3e0a42699174f4e3d65b51b3
tom4649 <tkaaynakiyoshi@gmail.com>
2026-05-16 18:00:48 +0900

    217. Contains Duplicate

---
+++ 0217.Contains-Duplicate/step2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+        return False
