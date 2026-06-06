ee68b3c6c9bb967ea6ea84d073b77a8a1b6ffd3b
tom4649 <tkaaynakiyoshi@gmail.com>
2026-06-06 09:07:12 +0900

    Add suggested changes

--- 0084.Largest-Rectangle-in-Histogram/step2_revised.py
+++ 0084.Largest-Rectangle-in-Histogram/step2_revised.py
@@ -1,13 +1,11 @@
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
-        increasing_stack: list[
-            tuple[int, int]
-        ] = []  # list of (height, num_greater_left)
+        increasing_stack: list[tuple[int, int]] = []  # (バーの高さ, 左側に広がる自分以上の合計幅)のリスト
         largest_area = 0
         heights.append(0)  # sentinel
 
         for height in heights:
-            num_greater_right = 0
+            num_greater_right = 0  # ポップされるバーから見て右側に広がる自分以上の合計幅
             while increasing_stack and height <= increasing_stack[-1][0]:
                 height_of_rectangle, num_greater_left = increasing_stack.pop()
                 largest_area = max(
@@ -16,6 +14,6 @@ class Solution:
                 )
                 num_greater_right += num_greater_left + 1
 
-            increasing_stack.append((height, num_greater_right))
+            increasing_stack.append((height, num_greater_right))  # 回収した右の幅がこのバーの左の幅
 
         return largest_area
