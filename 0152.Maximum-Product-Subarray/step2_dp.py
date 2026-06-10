e2b6d581eef20d1fbcd1466847e623531e391e94
tom4649 <tkaaynakiyoshi@gmail.com>
2026-06-10 19:26:26 +0900

    Fix typo

--- 0152.Maximum-Product-Subarray/step2_dp.py
+++ 0152.Maximum-Product-Subarray/step2_dp.py
@@ -2,14 +2,14 @@ class Solution:
     def maxProduct(self, nums: list[int]) -> int:
         max_product = nums[0]
         sub_max_product = nums[0]
-        sub_min_prduct = nums[0]
+        sub_min_product = nums[0]
 
         for n in nums[1:]:
             if n < 0:
-                sub_max_product, sub_min_prduct = sub_min_prduct, sub_max_product
+                sub_max_product, sub_min_product = sub_min_product, sub_max_product
 
             sub_max_product = max(n, sub_max_product * n)
-            sub_min_prduct = min(n, sub_min_prduct * n)
+            sub_min_product = min(n, sub_min_product * n)
 
             max_product = max(max_product, sub_max_product)
 
